d8943bbb64b81d70aa37c0864a950a8d46684580
QuotunicGit <cyr06899@gmail.com>
2025-05-04 16:09:07 +0800

    Commit 1

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+function login() {
+    let name = document.getElementById("login-name").value
+    let surname = document.getElementById("login-surname").value
+    let email = document.getElementById("login-email").value
+    let message = document.getElementById("login-message").value
+}
+
+function generate_code() {
+    return;
+}
