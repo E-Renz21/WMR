b958774a3e1ca0eacd94b1e80322c21f748d0839
E-Renz21 <e.renz.obguia@gmail.com>
2025-05-05 13:52:10 +0800

    asd

--- src/php/index.php
+++ src/php/index.php
@@ -1,3 +1,19 @@
-<?php
-echo "Lorem Ipsum!";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/phpStyle.css">
+    <title>Document</title>
+</head>
+<body>
+    <header class="nav">
+        <img src="../images/logo.png" class="logo" alt="Logo">
+        <li>
+            <ul><a href="">Request Delivery</a> </ul>
+            <ul><a href="">Trucks</a></ul>
+            <ul><a href="">About Us</a></ul>
+        </li>
+    </header>
+</body>
+</html>
